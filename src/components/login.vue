d80d48af1ac5a6cb5fdb830a73b787304c9ac73d
Cinderellazx <cinderella_bj@sina.com>
2018-10-31 15:45:36 +0800

    初始化

---
+++ src/components/login.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <h2>用户登录</h2>
+    <el-form label-position="top" label-width="80px" :model="formdata">
+        <el-form-item label="用户名">
+            <el-input v-model="formdata.name"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+            <el-input type="password" v-model="formdata.region"></el-input>
+        </el-form-item>
+        <el-button type="primary">登录</el-button>
+    </el-form>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      formdata: {
+        name: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
